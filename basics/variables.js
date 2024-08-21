5663ba689e2e06c52f65045fe8273f8068b3d4d0
PathFinderAditi <143380732+PathFinderAditi@users.noreply.github.com>
2024-08-21 18:17:03 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const a=100
+var city="Lucknow"
+let Name="aditi"
+let password="123"
+gmail="aditi@google.com"
+let state
+// a=200;
+city="Delhi"
+// console.log(a); //not allowed
+console.table([a,city,Name,password,gmail,state])
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
