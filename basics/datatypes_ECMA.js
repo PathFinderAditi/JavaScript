0a8776ff1e2b14080dcc68cdb41fb10e5ff30461
PathFinderAditi <143380732+PathFinderAditi@users.noreply.github.com>
2024-08-21 18:55:37 +0000

    container config done

---
+++ basics/datatypes_ECMA.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+let age=21
+let Name="aditi"
+//alert(3+4) //use it on browser, not on nodejs
+//code readbility should be high
+//number => 2 to 2^53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => used in figma, react for unique component
+
+//object
+console.log(typeof Name)
+console.log(typeof null)
+console.log(typeof undefined)
